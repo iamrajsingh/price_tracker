9a40da5f5a434fcbf7ca6921ab07c7cc61862d12
iamrajsingh <divyraj65407@gmail.com>
2023-10-27 22:35:09 +0530

    Modal done

---
+++ components/Modal.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import { useState, Fragment, FormEvent } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import Image from "next/image";
+
+const Modal = () => {
+  let [isOpen, setIsOpen] = useState(true);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    // await addUserEmailToProduct(productId, email)
+
+    setIsSubmitting(false);
+    setEmail('');
+    closeModal();
+  }
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+  return (
+    <>
+      <button type="button" className="btn" onClick={openModal}>
+        Track
+      </button>
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" onClose={closeModal} className="dialog-container">
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Overlay className="fixed inset-0" />
+            </Transition.Child>
+
+            <span className="inline-block h-screen align-middle" aria-hidden="true"></span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="dialog-content">
+                <div className="flex flex-col">
+                  <div className="flex justify-between">
+                    <div className="p-3 border border-x-gray-200 rounded-10">
+                    <Image src='/assets/icons/logo.svg' alt="logo" width={28} height={28}/>
+                    </div>
+                    <Image 
+                    src="/assets/icons/x-close.svg"
+                    alt="close"
+                    width={24}
+                    height={24}
+                    onClick={closeModal}
+                    className="cursor-pointer"
+                    />
+                  </div>
+                  <h4 className="dialog-head_text">
+                    Stay updated with product pricing alerts!
+                  </h4>
+                  <p className="text-sm text-gray-600 mt-2">
+                    Get notified when the price get changed!
+                  </p>
+                </div>
+                <form className="flex flex-col mt-5" onSubmit={handleSubmit}>
+                    <label htmlFor="email" className="text-sm font-medium text-gray-700">
+                        Email Address
+                    </label>
+                    <div className="dialog-input_container">
+                        <Image
+                          src="/assets/icons/mail.svg"
+                          alt="mail"
+                          width={20}
+                          height={20}
+                        />
+                        <input type="email"
+                        required
+                        id="email"
+                        placeholder="Enter Your Email Address"
+                        className="dialog-input"
+                        value={email}
+                        onChange={(e)=> setEmail(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit" className="dialog-btn">
+                        {isSubmitting ? 'Submitting...' : 'Track'}
+                    </button>
+                </form>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+};
+
+export default Modal;
