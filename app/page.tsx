1e8b5c7a0d6f56ddc2f18b1e84c62010aa82e90b
iamrajsingh <divyraj65407@gmail.com>
2023-10-29 23:11:28 +0530

    bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ const home = async () => {
         <h2 className="section-text">Trending</h2>
         <div className="flex flex-wrap gap-x-8 gap-y-16">
           {allProducts?.map((product)=> (
-              <ProductCard key={product._id} product={...product}/>
+              <ProductCard key={product._id} product={product}/>
           ))}
         </div>
       </section>
